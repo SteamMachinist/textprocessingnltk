8132d8479b6d3a9785a32bf7d8d4738e97f4947c
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2022-07-02 22:04:42 +0300

    added .csv download and autocleanup

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import uuid
+import csv
 from collections import defaultdict
 
 import nltk
 import fitz
 import pymorphy2
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 
@@ -27,12 +28,9 @@ stemmer_rus = SnowballStemmer("russian")
 morph = pymorphy2.MorphAnalyzer()
 
 
-def get_all_text(folder):
-    folder_path = os.path.join(app.config['UPLOAD_FOLDER'], folder)
-    file_list = os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], folder))
+def get_all_pdf_text(folder_path, files_list):
     all_text = ""
-
-    for file in file_list:
+    for file in files_list:
         with fitz.open(os.path.join(folder_path, file)) as pdf:
             for page in pdf:
                 all_text += page.get_text()
@@ -50,24 +48,53 @@ def processed_tokens_to_sorted_dictionary(processed_tokens: list) -> dict:
     return dict(sorted(token_dict.items(), key=lambda item: item[1], reverse=True))
 
 
-@app.route('/result/s/<folder>')
-def get_stemming_result(folder: str):
-    tokenized = tokenize(get_all_text(folder))
-    stemmed_tokens = [stemmer_rus.stem(token) for token in tokenized]
-    return render_template('result.html',
-                           process_type="стемминга",
-                           files_list=str(os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], folder))),
-                           tokens=processed_tokens_to_sorted_dictionary(stemmed_tokens))
+def process(files_list, folder_path: str, folder: str, process_type: str):
+    files_list.remove('result.csv') if 'result.csv' in files_list else None
+    files_list.remove('processed.txt') if 'processed.txt' in files_list else None
+
+    tokenized = tokenize(get_all_pdf_text(folder_path, files_list))
+
+    if process_type == 'lem':
+        processed_tokens = [morph.parse(token)[0].normal_form for token in tokenized]
+    elif process_type == 'stem':
+        processed_tokens = [stemmer_rus.stem(token) for token in tokenized]
+    else:
+        processed_tokens = tokenized
+
+    with open(os.path.join(folder_path, 'processed.txt'), mode='w+', encoding="utf-8") as processed:
+        processed.write('\n'.join(files_list))
+    with open(os.path.join(folder_path, 'result.csv'), mode='w+', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+        csv_writer.writerows(processed_tokens_to_sorted_dictionary(processed_tokens).items())
+
+    if folder != 'example':
+        for file in files_list:
+            os.remove(os.path.join(folder_path, file))
 
 
-@app.route('/result/l/<folder>')
-def get_lemmatization_results(folder: str):
-    tokenized = tokenize(get_all_text(folder))
-    lemmatized = [morph.parse(token)[0].normal_form for token in tokenized]
+@app.route('/result/<folder>/<process_type>', methods=['GET'])
+def get_result(folder: str, process_type: str):
+    folder_path = os.path.join(app.config['UPLOAD_FOLDER'], folder)
+    files_list = os.listdir(folder_path)
+    if 'result.csv' not in files_list or folder == 'example':
+        process(files_list, folder_path, folder, process_type)
+
+    with open(os.path.join(folder_path, 'processed.txt'), mode='r', encoding="utf-8") as processed:
+        files_list = ', '.join(processed.read().split('\n'))
+    with open(os.path.join(folder_path, 'result.csv'), mode='r', encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        tokens_with_amount = [(row[0], row[1]) for row in csv_reader]
+
     return render_template('result.html',
-                           process_type="лемматизации",
-                           files_list=str(os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], folder))),
-                           tokens=processed_tokens_to_sorted_dictionary(lemmatized))
+                           files_list=files_list,
+                           folder=folder,
+                           process_type=process_type,
+                           tokens_with_amount=tokens_with_amount)
+
+
+@app.route('/result/<folder>/<process_type>/download', methods=['GET'])
+def download_result(folder: str, process_type: str):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], folder, 'result.csv'), as_attachment=True)
 
 
 def is_valid(filename: str) -> bool:
@@ -93,11 +120,7 @@ def upload_files_index():
         for file in files:
             file.save(os.path.join(folder_path, file.filename))
 
-    process_type = request.form['process_type']
-    if process_type == 'stemming':
-        return redirect(url_for('get_stemming_result', folder=folder))
-    else:
-        return redirect(url_for('get_lemmatization_results', folder=folder))
+    return redirect(url_for('get_result', folder=folder, process_type=request.form['process_type']))
 
 
 if __name__ == '__main__':
